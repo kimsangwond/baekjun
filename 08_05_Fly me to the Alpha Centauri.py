4f3e74a647aadbd5f648ac06ca9998447441ce55
kimsangwon <tkddnjs2014@naver.com>
2019-09-28 21:45:25 +0900

    08_07

--- 08_05_Fly me to the Alpha Centauri.py
+++ 08_05_Fly me to the Alpha Centauri.py
@@ -1,17 +1,4 @@
-def calculate(distance):
-    n = 1
-    while distance >= n * (n + 1):
-        n += 1
-    num = n - 1
 
-    if distance==1:
-        return 1
-    elif distance==2:
-        return 2
-    elif (distance - num * (num + 1)) > num + 1:
-        return 2*num + 2
-    else:
-        return 2*num + 1
 
 if __name__ == '__main__':
     count = int(input())
